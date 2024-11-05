9862deef61ba31c633754975e9bf0fed2d46154b
tennylaylor <Lennyttaylor@gmail.com>
2024-11-04 22:10:59 -0500

    update no blue nav bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import ".styles.css";
-// Step 1: Select and cache the <main> element in a variable named mainEl.
+
 const mainEl = document.querySelector('main');
 
-// Step 2: Set the background color of mainEl to the --main-bg CSS custom property.
+
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
-// Step 3: Set the content of mainEl to "<h1>DOM Manipulation</h1>".
+
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 
 // Step 4: Add a class of flex-ctr to mainEl.
 mainEl.classList.add('flex-ctr');
+
+// New code for the <nav> element manipulation
+const topMenuEl = document.getElementById('top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');

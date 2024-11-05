87ee3a756081c5b9004fb969fb1d4b59cc48fffb
tennylaylor <Lennyttaylor@gmail.com>
2024-11-04 23:06:31 -0500

    Udate menulink

--- src/index.js
+++ src/index.js
@@ -16,3 +16,17 @@ const topMenuEl = document.getElementById('top-menu');
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add('flex-around');
+
+const menuLinks = [
+    {href: '#home', text:'home'},
+    {href: '#about', text:'about'},
+    {href: '#services', text:'services'},
+    {href: '#contact', text:'contact'},
+];
+
+menuLinks.forEach(link => {
+    const linkElement = document.createElement('a'); 
+    linkElement.href = link.href;                    
+    linkElement.textContent = link.text;            
+    topMenuEl.appendChild(linkElement);              
+  });
